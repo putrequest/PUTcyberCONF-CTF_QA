c4e41623ccbcad9f7f97c3bf91362c0e3a6bde6c
Mateusz Szuda <64504618+mattix1710@users.noreply.github.com>
2024-04-23 22:18:10 +0200

    Added dark view and set to main view

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from PyQt6.QtWidgets import QApplication
 import sys
+import qdarkstyle
 
 import windowInit
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     
-    window = windowInit.MainWindow()
+    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt6())
+    # app.setStyleSheet("")
+    
+    window = windowInit.MainWindow(app)
     window.show()
     sys.exit(app.exec())
--- windowInit.py
+++ windowInit.py
@@ -1,23 +1,37 @@
-from PyQt6.QtWidgets import QVBoxLayout
+from PyQt6.QtWidgets import QVBoxLayout, QApplication
 from PyQt6 import QtWidgets as widget
 # local imports
 import gui, functions
+import qdarkstyle
 
 class MainWindow(widget.QMainWindow):
     
-    def __init__(self):
+    def __init__(self, app: QApplication):
         super(MainWindow, self).__init__()
         self.ui = gui.Ui_CTF_QA()
         self.ui.setupUi(self)
         
         self.setFixedSize(self.size())
         
-        self.UI_manager()
+        self.UI_manager(app)
+        self.program_logic()
         self.show()
 
-    def UI_manager(self):
+    def UI_manager(self, app: QApplication):
         # SET MENU BAR
         self.ui.action_reset_progress.triggered.connect(functions.reset_progress)
+        # appearance
+        def set_appearance_dark():
+            app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt6())
+            self.ui.action_appearance_light.setChecked(False)
+            self.ui.action_appearance_dark.setChecked(True)
+        def set_appearance_light():
+            app.setStyleSheet("")
+            self.ui.action_appearance_light.setChecked(True)
+            self.ui.action_appearance_dark.setChecked(False)
+            
+        self.ui.action_appearance_dark.triggered.connect(set_appearance_dark)
+        self.ui.action_appearance_light.triggered.connect(set_appearance_light)
         
         font = self.ui.title.font()
         font.setBold(True)
@@ -26,5 +40,7 @@ class MainWindow(widget.QMainWindow):
         self.ui.title.setFont(font)
         
         self.task_layout = QVBoxLayout(self.ui.scroll_area_tasks_widget)
-        functions.populate_task_list(self.task_layout, self.ui.title)
         self.task_layout.addStretch()
+        
+    def program_logic(self):
+        functions.populate_task_list(self.task_layout, self.ui.title)
